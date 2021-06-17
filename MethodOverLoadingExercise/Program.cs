844b2191c3471d3839eaecc367a50d8170daf76c
Mark Anderson <markanderson8162@gmail.com>
2021-06-17 13:52:31 -0500

    created method overloading exercise

---
+++ MethodOverLoadingExercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MethodOverLoadingExercise
+{
+	class Program
+	{
+
+		public static int Add(int a, int b)
+		{
+			return a + b;
+		}
+
+		public static double Add(double a, double b)
+		{
+			return a + b;
+		}
+
+		public static string Add(int a, int b, bool isTrue)
+		{
+			int sumAB = a + b;
+			char letterS;
+			if(isTrue == true && sumAB > 1)
+			{
+				letterS =  's';
+			}
+			else
+			{
+				letterS = ' ';
+			}
+			return $"{sumAB} dollar{letterS}";
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Add(5, 3, true));
+			Console.WriteLine(Add(0, 1, true));
+		}
+	}
+}
